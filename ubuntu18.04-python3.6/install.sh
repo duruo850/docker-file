a14d0e43c9f373d669bdaeb8873aacec35b60b55
zhanchenjin <zhanchenjin@magefitness.com>
2018-07-12 17:48:17 +0800

    * 修改文件

--- ubuntu18.04-python3.6/install.sh
+++ ubuntu18.04-python3.6/install.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-echo $0
-
 export py=python
 
 # 安装pyhon3.6.5
-sudo apt-get update
-sudo apt-get install --yes python3.6
-sudo ln -sf /usr/bin/python3.6 /usr/bin/python3
-sudo ln -sf /usr/bin/python3 /usr/bin/python
-sudo ln -sf /usr/bin/pip3 /usr/bin/pip
-sudo apt-get install --yes python3-dev
-sudo apt-get install --yes libpython3.6-dev
-sudo apt-get install --yes python3-pip
-sudo pip3 install --upgrade pip
+apt-get update
+apt-get install --yes python3.6
+ln -sf /usr/bin/python3.6 /usr/bin/python3
+ln -sf /usr/bin/python3 /usr/bin/python
+ln -sf /usr/bin/pip3 /usr/bin/pip
+apt-get install --yes python3-dev
+apt-get install --yes libpython3.6-dev
+apt-get install --yes python3-pip
+pip3 install --upgrade pip
 
 # 解决ModuleNotFoundError: No module named 'pip._internal'
-sudo python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
-sudo apt-get install --yes libmysqld-dev
-sudo apt-get install --yes uwsgi-plugin-python
-sudo apt-get install --yes swig
-sudo apt-get install --yes libssl-dev
-sudo apt-get install --yes pylint
-sudo apt-get install --yes mysql-client
-sudo apt-get install --yes libfreetype6-dev
-sudo apt-get install --yes libjpeg-dev
-sudo apt-get install --yes libxml2-dev libxslt-dev
-sudo apt-get install --yes python-yaml
-sudo apt-get install --yes python-lxml
-sudo apt-get install --yes libyaml-cpp-dev
-sudo apt-get install --yes libffi-dev libssl-dev
-sudo ln -sf /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
-sudo pip3 install -r `dirname ${BASH_SOURCE[0]}`/requirements.txt
+apt-get install --yes libmysqld-dev
+apt-get install --yes uwsgi-plugin-python
+apt-get install --yes swig
+apt-get install --yes libssl-dev
+apt-get install --yes pylint
+apt-get install --yes mysql-client
+apt-get install --yes libfreetype6-dev
+apt-get install --yes libjpeg-dev
+apt-get install --yes libxml2-dev libxslt-dev
+apt-get install --yes python-yaml
+apt-get install --yes python-lxml
+apt-get install --yes libyaml-cpp-dev
+apt-get install --yes libffi-dev libssl-dev
+ln -sf /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+pip3 install -r `dirname ${BASH_SOURCE[0]}`/requirements.txt
